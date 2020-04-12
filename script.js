6f39b5300f00b19c23427a81ba9cdbdbcd25e274
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-04-12 06:16:39 +0100

    added contact form js vlaidation

--- script.js
+++ script.js
@@ -33,7 +33,29 @@ function showSlides(n) {
   
 }
 
-function myFunction()
-		{
-		alert("Your message has been sent");
-		}
+function sendingFunction()	{
+      valid = true;
+  
+      if ( document.forms["contact_form"]["name"].value == "" )
+      {
+          alert ( "Please type in your name." );
+          valid = false;
+      } else if( document.forms["contact_form"]["email"].value == "" )
+      {
+          alert ( "Please type in your email." );
+          valid = false;
+      } else if( document.forms["contact_form"]["subject"].value == "reset" )
+      {
+          alert ( "Please select your subject." );
+          valid = false;
+      } else if( document.forms["contact_form"]["feedback"].value == "" )
+      {
+          alert ( "Please type in your message." );
+          valid = false;
+      } else{
+          alert (" We will contact you as soon as possible")
+          valid = true;
+      }
+  
+      return valid;
+}
