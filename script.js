4108a8b81c4f26e10cbfb328528c85dfacda3f7b
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-04-20 02:23:01 +0100

    wrapped logo in a tag which is linking to main page

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-
 // Form Script
 
-function sendingFunction()	{
-      valid = true;
-  
-      if ( document.forms["contact_form"]["name"].value == "" )
-      {
-          alert ( "Please type in your name." );
-          valid = false;
-      } else if( document.forms["contact_form"]["email"].value == "" )
-      {
-          alert ( "Please type in your email address." );
-          valid = false;
-      } else if( document.forms["contact_form"]["subject"].value == "reset" )
-      {
-          alert ( "Please select your subject." );
-          valid = false;
-      } else if( document.forms["contact_form"]["feedback"].value == "" )
-      {
-          alert ( "Please type in your message." );
-          valid = false;
-      } else{
-          alert (" We will contact you as soon as possible")
-          valid = true;
-      }
-  
-      return valid;
+function sendingFunction() {
+  valid = true;
+
+  if (document.forms["contact_form"]["name"].value == "") {
+    alert("Please type in your name.");
+    valid = false;
+  } else if (document.forms["contact_form"]["age"].value == "") {
+    alert("Please type in your date of birth");
+    valid = false;
+  } else if (document.forms["contact_form"]["gender"].value == "") {
+    alert("Please choose your gender");
+    valid = false;
+  } else if (document.forms["contact_form"]["email"].value == "") {
+    alert("Please type in your email address.");
+    valid = false;
+  } else if (document.forms["contact_form"]["phone"].value == "") {
+    alert("Please type in your phone number.");
+    valid = false;
+  } else if (document.forms["contact_form"]["subject"].value == "reset") {
+    alert("Please select your subject.");
+    valid = false;
+  } else if (document.forms["contact_form"]["feedback"].value == "") {
+    alert("Please type in your message.");
+    valid = false;
+  } else {
+    alert(" We will contact you as soon as possible")
+    valid = true;
+  }
+
+  return valid;
 }
 
 // Menu Script
 
-function toggleMenu(){
+function toggleMenu() {
   var navbar = document.getElementById("navbar");
-  if (navbar.style.display ==="none"){
-    navbar.style.display="block";
-  }else{
-    navbar.style.display="none";
-  } 
+  if (navbar.style.display === "none") {
+    navbar.style.display = "block";
+  } else {
+    navbar.style.display = "none";
+  }
 }
